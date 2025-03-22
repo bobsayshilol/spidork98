1a27c4cf524475248c4786764a9458ae4ca20539
bobsayshilol <bobsayshilol@live.co.uk>
2025-03-22 23:54:15 +0000

    Fixup makefiles so that they work from a fresh setup

--- x/examples/libpc98/flags.mk
+++ x/examples/libpc98/flags.mk
@@ -7,10 +7,11 @@ STRIP	= strip.exe
 
 # Compiler options
 CFLAGS	= -g -Wall -Werror
+#CFLAGS	+= -mcpu=i486 -march=i486 -O3
 CFLAGS	+= -mcpu=i386 -march=i386 -O3
 
 # LIBPC98 target
 LIBPC98	= ..\\libpc98\\libpc98.a
 CFLAGS	+= -I..\\libpc98
 LDFLAGS	=
-DEPS	= ..\\libpc98\\funcs.h ..\\libpc98\\gpuscrn.h $(LIBPC98)
+DEPS	= ..\\libpc98\\funcs.h ..\\libpc98\\gpuscrn.h
