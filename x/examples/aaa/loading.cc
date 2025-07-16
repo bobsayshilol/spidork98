7dd1f2dc41e9db02acb007fcf8cf3d8b1302b7c8
bobsayshilol <bobsayshilol@live.co.uk>
2025-07-16 23:07:51 +0100

    Small changes to the loading screen

--- x/examples/aaa/loading.cc
+++ x/examples/aaa/loading.cc
@@ -1,6 +1,7 @@
 #include "loading.h"
 
 #include "funcs.h"
+#include "gpuscrn.h"
 #include "utils.h"
 #include "types.h"
 
@@ -12,8 +13,17 @@
 
 namespace game {
 
+#if 1
+#define LEFT_2CHAR " ["
+#define RIGHT_2CHAR "] "
+#else
+#define LEFT_2CHAR "\x81\x73" // <<
+#define RIGHT_2CHAR "\x81\x74" // >>
+#endif
+
 void loading_screen(void (*tick_progress)(LoadingProgress::E progress)) {
   // Remove any text.
+  gpu::enable_text_layer(true);
   Funcs98::clear_screen();
 
   // Show something to confirm that it's actually running.
@@ -27,27 +37,39 @@ void loading_screen(void (*tick_progress)(LoadingProgress::E progress)) {
   // Can't do much with attributes.
   //ScreenAttrib = GREEN;
 
-  {
-    const char first_line[] = "S.P.I.D.O.R.K. 0.9.8 INITIALISING";
-    const int spacing = (num_cols - sizeof(first_line)) / 2;
-    ScreenPutString_98(first_line, GREEN, spacing, num_rows / 3);
-  }
+  // Info line bits.
+  const char info_line[] = "\xA2 S.P.I.D.O.R.K. 0.9.8 INITIALISING \xA3";
+  const int info_line_x = (num_cols - sizeof(info_line)) / 2;
+  const int info_line_y = num_rows / 3;
+  ScreenPutString_98(info_line, GREEN, info_line_x, info_line_y);
+  //const char loading_chars_l[] = "|/-`";
+  //const char loading_chars_r[] = "|`-/";
 
+  // Progress bar stuff.
   const u8 pb_x = num_cols / 4;
   const u8 pb_y = num_rows / 2;
   const u8 pb_w = num_cols / 2;
 
+  // yen symbol
+  const char fill_char = 0x5C; //(rand() & 0x10) ? '#' : 0x5C;
+
   // Progress bar.
   tick_progress(LoadingProgress::BarStart);
   {
-    ScreenPutChar_98('[', GREEN, pb_x - 1, pb_y);
-    ScreenPutChar_98(']', GREEN, pb_x + pb_w, pb_y);
+    //u8 loading_char_idx = 0;
+
+    ScreenPutString_98(LEFT_2CHAR, GREEN, pb_x - 2, pb_y);
+    ScreenPutString_98(RIGHT_2CHAR, GREEN, pb_x + pb_w, pb_y);
     for (u8 i = 0; i < pb_w; i++) {
-      tick_progress(LoadingProgress::BarTick);
-      Funcs98::delay_ms(50);
-      tick_progress(LoadingProgress::BarTick);
-      Funcs98::delay_ms(50);
-      ScreenPutChar_98('#', GREEN, pb_x + i, pb_y);
+      for (int j = 0; j < 2; j++) {
+        //ScreenPutChar_98(loading_chars_l[loading_char_idx], GREEN, info_line_x - 2, info_line_y);
+        //ScreenPutChar_98(loading_chars_r[loading_char_idx], GREEN, info_line_x + sizeof(info_line) + 1, info_line_y);
+        //loading_char_idx = (loading_char_idx + 1) & 3;
+        Funcs98::delay_ms(50);
+        tick_progress(LoadingProgress::BarTick);
+      }
+
+      ScreenPutChar_98(fill_char, GREEN, pb_x + i, pb_y);
     }
   }
 
@@ -75,12 +97,13 @@ void loading_screen(void (*tick_progress)(LoadingProgress::E progress)) {
       num_hs--;
       memmove(heights + idx, heights + idx + 1, (num_hs - idx) * sizeof(ChPos));
     } else {
-      ScreenPutChar_98('#', GREEN, pos.x, pb_y + pos.dy);
-      ScreenPutChar_98('#', GREEN, pos.x, pb_y - pos.dy);
+      ScreenPutChar_98(fill_char, GREEN, pos.x, pb_y + pos.dy);
+      ScreenPutChar_98(fill_char, GREEN, pos.x, pb_y - pos.dy);
     }
 
     // Spread them out.
     do_next++;
+    if (do_next == 20) tick_progress(LoadingProgress::FadeOutTick);
     if (do_next > 40) { // ~80ms
       do_next = 0;
       tick_progress(LoadingProgress::FadeOutTick);
@@ -89,13 +112,13 @@ void loading_screen(void (*tick_progress)(LoadingProgress::E progress)) {
         ChPos & chl = heights[num_hs];
         chl.x = pb_x - 1 - next_dx;
         chl.dy = 0;
-        ScreenPutChar_98('#', GREEN, chl.x, pb_y);
+        ScreenPutChar_98(fill_char, GREEN, chl.x, pb_y);
         num_hs++;
 
         ChPos & chr = heights[num_hs];
         chr.x = pb_x + pb_w + next_dx;
         chr.dy = 0;
-        ScreenPutChar_98('#', GREEN, chr.x, pb_y);
+        ScreenPutChar_98(fill_char, GREEN, chr.x, pb_y);
         num_hs++;
 
         next_dx++;
