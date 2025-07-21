d9fec6274e16f9cc089d8cb209a5ed221c377fe9
bobsayshilol <bobsayshilol@live.co.uk>
2025-07-21 22:14:32 +0100

    Output benchmarks to a file

--- x/examples/bench/bench.cc
+++ x/examples/bench/bench.cc
@@ -1,4 +1,5 @@
 #include "bench.h"
+#include "logs.h"
 
 #include <cstdio>
 
@@ -11,22 +12,29 @@ Benchmark *s_active;
 } // namespace
 
 void run_all() {
+  printf("Report will be written to bench.txt\n");
+  logging::init("bench.txt");
+
   Benchmark *info = s_active;
   while (info != NULL) {
     printf("Running %s:\n", info->name);
     info->func(*info);
     info = info->next;
+break;
   }
+
+  printf("Finished\n");
 }
 
 Timer::Timer(const char *n, int i) : name(n), max(i), counter(0) {
   start = Funcs98::ticks();
+  printf("  - %s\n", n);
 }
 
 Timer::~Timer() {
   uclock_t stop = Funcs98::ticks();
   uclock_t ticks = stop - start;
-  printf("  %s: %lld ticks/iter (%lld total)\n", name, ticks / max, ticks);
+  logging::print(logging::Level::Info, "  %s: %lld ticks/iter (%lld total)", name, ticks / max, ticks);
 }
 
 int registr(Benchmark *info) {
