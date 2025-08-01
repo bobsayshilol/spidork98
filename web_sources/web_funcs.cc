3c26f1d68f65686cc2ba0a9d9a1c369b21c089a5
bobsayshilol <bobsayshilol@live.co.uk>
2025-08-01 20:59:48 +0100

    Get a window and basic input working

--- web_sources/web_funcs.cc
+++ web_sources/web_funcs.cc
@@ -2,6 +2,14 @@
 #include "keyboard.h"
 
 #include "web_common.h"
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_keyboard.h>
+
+namespace {
+
+const bool *s_sdl_keys;
+
+} // namespace
 
 void ScreenPutString_98(const char *text, unsigned colour, int x, int y) {
   // TODO
@@ -31,20 +39,56 @@ void Funcs64::pc_beep(int freq) {
 }
 
 bool Funcs64::kb_hit() {
-  // TODO
-  return false;
+  // Refresh the keyboard state.
+  SDL_PumpEvents();
+  s_sdl_keys = SDL_GetKeyboardState(nullptr);
+  return getch_98();
 }
 
 //
 
 int getch_98() {
-  // TODO
+  if (!s_sdl_keys) {
+    return 0;
+  }
+
+  // This is called right after kb_hit() so just read it off.
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_Q]) return 'q';
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_W]) return 'w';
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_E]) return 'e';
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_T]) return 't';
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_A]) return 'a';
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_S]) return 's';
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_D]) return 'd';
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_RETURN]) return '\r';
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_SPACE]) return ' ';
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_DOWN]) return 10;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_UP]) return 11;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_LEFT]) return 8;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_RIGHT]) return 12;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_ESCAPE]) return 27;
   return 0;
 }
 
 //
 
 u32 read_keyboard_state() {
-  // TODO
-  return 0;
+  // Refresh current state.
+  Funcs64::kb_hit();
+
+  u32 bits = 0;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_Q]) bits |= KB_STATE_Q;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_W]) bits |= KB_STATE_W;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_E]) bits |= KB_STATE_E;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_T]) bits |= KB_STATE_T;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_A]) bits |= KB_STATE_A;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_S]) bits |= KB_STATE_S;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_D]) bits |= KB_STATE_D;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_RETURN]) bits |= KB_STATE_ENTER;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_SPACE]) bits |= KB_STATE_SPACE;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_DOWN]) bits |= KB_STATE_DOWN;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_UP]) bits |= KB_STATE_UP;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_LEFT]) bits |= KB_STATE_LEFT;
+  if (s_sdl_keys[SDL_Scancode::SDL_SCANCODE_RIGHT]) bits |= KB_STATE_RIGHT;
+  return bits;
 }
--- web_sources/web_gpu.cc
+++ web_sources/web_gpu.cc
@@ -1,22 +1,113 @@
 #include "gpuscrn.h"
+#include "logs.h"
 
 #include "web_common.h"
 
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_pixels.h>
+#include <SDL3/SDL_surface.h>
+#include <SDL3/SDL_video.h>
+
+#include <array>
+#include <memory>
+
 namespace gpu {
+
+namespace {
+
+struct SDLDeleter {
+  void operator()(SDL_Palette *p) {
+    SDL_DestroyPalette(p);
+  }
+  void operator()(SDL_Surface *s) {
+    SDL_DestroySurface(s);
+  }
+  void operator()(SDL_Window *w) {
+    SDL_DestroyWindow(w);
+  }
+};
+
+constexpr std::size_t MAX_PALETTE_SIZE = 256;
+
+std::unique_ptr<SDL_Window, SDLDeleter> s_window;
+std::unique_ptr<SDL_Palette, SDLDeleter> s_palette;
+std::unique_ptr<SDL_Surface, SDLDeleter> s_back_buffer;
+std::unique_ptr<SDL_Surface, SDLDeleter> s_front_buffer;
+
+} // namespace
 
 DrawTo::E g_draw_to;
 
 FASTCALL bool setup() {
-  // TODO
-  return false;
+  if (SDL_WasInit(SDL_INIT_VIDEO)) {
+    logging::print(logging::Level::Warning, "GPU already setup");
+    return false;
+  }
+
+  if (!SDL_InitSubSystem(SDL_INIT_VIDEO)) {
+    logging::print(logging::Level::Error, "Failed to init video: %s", SDL_GetError());
+    return false;
+  }
+
+  s_window.reset(SDL_CreateWindow("SPIDORK98", GPU_WIDTH, GPU_HEIGHT, 0));
+  if (!s_window) {
+    logging::print(logging::Level::Error, "Failed to create window: %s", SDL_GetError());
+    return false;
+  }
+
+  s_palette.reset(SDL_CreatePalette(MAX_PALETTE_SIZE));
+  if (!s_palette) {
+    logging::print(logging::Level::Error, "Failed to create palette: %s", SDL_GetError());
+    return false;
+  }
+
+  s_front_buffer.reset(SDL_CreateSurface(GPU_WIDTH, GPU_HEIGHT, SDL_PIXELFORMAT_RGBX8888));
+  if (!s_front_buffer) {
+    logging::print(logging::Level::Error, "Failed to create front surface: %s", SDL_GetError());
+    return false;
+  }
+  s_back_buffer.reset(SDL_CreateSurface(GPU_WIDTH, GPU_HEIGHT, SDL_PIXELFORMAT_RGBX8888));
+  if (!s_back_buffer) {
+    logging::print(logging::Level::Error, "Failed to create back surface: %s", SDL_GetError());
+    return false;
+  }
+
+  SDL_Surface * window_surface = SDL_GetWindowSurface(s_window.get());
+  for (auto *surface : { window_surface, s_front_buffer.get(), s_back_buffer.get()}) {
+    if (!SDL_SetSurfacePalette(surface, s_palette.get())) {
+      logging::print(logging::Level::Error, "Failed to set palette on a surface: %s", SDL_GetError());
+      return false;
+    }
+  }
+
+  // Setup the default palette.
+  {
+    std::array<SDL_Color, MAX_PALETTE_SIZE> pal;
+    pal.fill({});
+    if (!SDL_SetPaletteColors(s_palette.get(), pal.data(), 0, pal.size())) {
+      logging::print(logging::Level::Error, "Failed to set palette colours: %s", SDL_GetError());
+      return false;
+    }
+  }
+
+  return true;
 }
 
 FASTCALL void shutdown() {
-  // TODO
+  if (!SDL_WasInit(SDL_INIT_VIDEO)) return;
+
+  s_back_buffer.reset();
+  s_front_buffer.reset();
+  s_window.reset();
+
+  SDL_QuitSubSystem(SDL_INIT_VIDEO);
 }
 
 FASTCALL void wait_for_vsync() {
-  // TODO
+  SDL_Surface * window_surface = SDL_GetWindowSurface(s_window.get());
+  SDL_BlitSurface(s_front_buffer.get(), nullptr, window_surface, nullptr);
+  SDL_UpdateWindowSurface(s_window.get());
 }
 
 FASTCALL void enable_text_layer(bool show) {
