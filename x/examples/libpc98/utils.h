a89e1aafc4da587540281558c4c81c58a3609f5c
bobsayshilol <bobsayshilol@live.co.uk>
2026-06-17 00:05:16 +0100

    Add utility rotate function

--- x/examples/libpc98/utils.h
+++ x/examples/libpc98/utils.h
@@ -36,6 +36,21 @@ static void randomise(T * p, int n) {
   }
 }
 
+template <int N, typename T>
+static void rotate_left(T * start, T * end) {
+  T tmp[N];
+  for (int i = 0; i < N; i++) {
+    tmp[i] = start[i];
+  }
+  T * mid = start + N;
+  while (mid != end) {
+    *start++ = *mid++;
+  }
+  for (int i = 0; i < N; i++) {
+    *start++ = tmp[i];
+  }
+}
+
 } // namespace utils
 
 #endif
